fd7cd2a3caee15e8253f2b6de764bc50dd78213c
Saranya Fungtammasan <yylttwns@gmail.com>
2023-01-13 14:43:36 +0700

    fix train data and move to jupyter notebook

--- main.py
+++ main.py
@@ -31,11 +31,14 @@ def readCsv(file_path):
         csv_reader = csv.reader(csv_file, delimiter=',')
         label = 0
         for row in csv_reader:
-            text = re.findall(
-                f'"selectedText"\:(.*?)\,', row[6])
-            if (row[5]) == "Other":
+            if (row[4] == "0"):
                 label = 1
+            else:
+                label = 0
+            text = re.findall(
+                f'"selectedText"\:(.*?)"\,', row[6])
             if len(text) > 0:
+             #   print((row[4] == "0"), label, text)
                 for t in text:
                     aDataset.append([label, t])
                     y.append(label)
@@ -49,7 +52,7 @@ for file in os.listdir():
         readCsv(file_path)
 
 df = pd.DataFrame(aDataset, columns=['label', 'text'])
-
+# print(df)
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=0)
 
@@ -57,6 +60,7 @@ text_transformer = TfidfVectorizer(stop_words='english', ngram_range=(
     1, 3), lowercase=True)
 X_train_text = text_transformer.fit_transform(X_train)
 X_test_text = text_transformer.transform(X_test)
+# print(y_test)
 
 logit = LogisticRegression(
     C=5e1, solver='lbfgs', multi_class='multinomial', random_state=17, n_jobs=4, max_iter=400)
@@ -64,3 +68,10 @@ skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=17)
 cv_results = cross_val_score(
     logit, X_train_text, y_train, cv=skf, scoring='f1_micro')
 print(cv_results, cv_results.mean())
+logit.fit(X_train_text, y_train)
+test_preds = logit.predict(X_test_text)
+""" pd.DataFrame(test_preds, columns=['label']).to_csv('logit_tf_idf_starter_submission.csv',
+                                                   index_label='id') """
+count = 0
+for i in range(test_preds):
+    print(test_preds[i], y_test[i])
